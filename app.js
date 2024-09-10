94248c0cc812fd26068db2af76de54974cf7b011
Swizel De Melo <swizeldemelo@gmail.com>
2024-09-10 10:55:10 +0100

    Added dark mode

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+const submit=document.getElementById("submit");
+const onMouseover=(element)=>{
+    element.style.border="1px solid black";
+  }
+  
+  const onMouseout=(element)=>{
+    element.style.border="none";
+  }
 
-    const images=document.querySelectorAll("img");
-    for(const image of images){
-        fetch("https://dog.ceo/api/breeds/image/random")
-        .then(response=>response.json())
-        .then(data=>{
-            image.src=data.message
-            image.height=100;
-            image.width=100;
-        })
+
+  function signUpMessage() {
+    const email = document.getElementById("email").value
+    
+    if (email !== "" && email.includes("@")) {
+        alert("Thanks for signing up!");
+    } else {
+        alert("Please enter a valid email.");
     }
-})
+}
+
+function toggleDarkMode() {
+  // Toggle the "dark-mode" class on the body
+  document.body.classList.toggle("dark-mode");
+  const elements = document.querySelectorAll(".main-news");
+  for (const element of elements) {
+    element.classList.toggle("myStyle");
+  }
+}
+
+/*#logo>a color: #cadbe9;*/
+/*#nav-ul>li>a #fff*/
+
+
